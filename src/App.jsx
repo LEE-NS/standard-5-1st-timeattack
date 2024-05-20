fe2cf90e0a3281b9a467281f0d678824925e8ea5
LEE-NS <ogawa9341@gmail.com>
2024-05-20 15:48:03 +0900

    Update complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react"
+
 export default function App() {
+  const [num, setNum] = useState(0)
+  const [result, setResult] = useState(0)
+
+  const onChangeHandler = (e) => {
+    setNum(e.target.value)
+  }
+
+  const plusHandler = (num) => {
+    setResult(result + Number(num))
+    setNum(0)
+  }
+
+  const minusHandler = (num) => {
+    setResult(result - Number(num))
+    setNum(0)
+  }
+
+  const resetNum = () => {
+    setNum(0)
+    setResult(0)
+  }
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input type="number" value={num} onChange={onChangeHandler} /> 만큼을{" "}
+        <button onClick={() => plusHandler(num)}>더할게요</button>{" "}
+        <button onClick={() => minusHandler(num)}>뺄게요</button>
+        <button onClick={resetNum}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{result}</p>
       </div>
     </div>
-  );
+  )
 }
